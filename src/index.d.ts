823b4b6c1e069c824be6e257bdf632a188de5493
Vincent Neo <23420208+vincentneo@users.noreply.github.com>
2022-09-22 00:37:45 +0800

    add typescript .d.ts

---
+++ src/index.d.ts
@@ -0,0 +1,18 @@
+
+interface Props<T> extends DOMAttributes<T> {
+    children: JSX.Element;
+    component?: string;
+    duration?: number;
+    disableInitialAnimation?: boolean;
+    className: string;
+    classNames?: {
+        beforeEnter: string,
+        entering: string,
+        beforeLeave: string,
+        leaving: string
+    }
+}
+
+declare module "react-tiny-crossfade" {
+    export default function TinyCrossfade(props: Props): JSX.Element
+}
